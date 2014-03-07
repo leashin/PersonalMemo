ab97689058780546187500de03befc9de187bf01
leashin <leashin@leashin-PC>
2014-03-07 10:14:24 +0800

    pix修改

--- src/com/leashin/personalmemo/utils/PixelUtils.java
+++ src/com/leashin/personalmemo/utils/PixelUtils.java
@@ -2,18 +2,14 @@ package com.leashin.personalmemo.utils;
 
 import android.content.Context;
 
-import com.leashin.personalmemo.MyApplication;
-
 public class PixelUtils {
 
-	public static int dp2px(float dpValue) {
-		final Context context = MyApplication.getInstance();
+	public static int dp2px(final Context context, final float dpValue) {
 		final float scale = context.getResources().getDisplayMetrics().density;
 		return (int) (dpValue * scale + 0.5f);
 	}
 
-	public static int px2dp(float pxValue) {
-		final Context context = MyApplication.getInstance();
+	public static int px2dp(final Context context, final float pxValue) {
 		final float scale = context.getResources().getDisplayMetrics().density;
 		return (int) (pxValue / scale + 0.5f);
 	}
